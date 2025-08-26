bfd7b712f2f003c5422bd8d3ef4e346c0417338d
lkm1401 <lkm1401@naver.com>
2025-08-26 23:38:17 +0900

    수업자료 커밋

---
+++ springboot/src/main/java/com/sch/springboot/controller/RestEmployeeController.java
@@ -0,0 +1,36 @@
+package com.sch.springboot.controller;
+
+import com.sch.springboot.dto.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000") // React 개발 서버 주소
+public class RestEmployeeController {
+
+    List<Employee> employees = new ArrayList<Employee>();
+
+    /**
+     * 사원 등록
+     */
+    @PostMapping("/employees/register")
+    public String register(@RequestBody Employee employee) {
+        System.out.println(employee.getName());
+        System.out.println(employee.getAddress());
+        employees.add(employee);
+
+        return "ok";
+    }
+
+    /**
+     * 사원 리스트 조회
+     */
+    @GetMapping("/employees")
+    public  List<Employee> list() {
+        System.out.println(employees.size());
+        return employees;
+    }
+}
---
+++ springboot/src/main/java/com/sch/springboot/dto/Employee.java
@@ -0,0 +1,31 @@
+package com.sch.springboot.dto;
+
+public class Employee {
+    private Long sno;
+    private String name;
+    private String address;
+
+    public Long getSno() {
+        return sno;
+    }
+
+    public void setSno(Long sno) {
+        this.sno = sno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
