637d43949073c542914d8400c797ca9a5ecf8f77
lee <lkm1401@naver.com>
2025-08-26 10:38:15 +0900

    2025-08-26 commit~

---
+++ springboot/src/main/java/com/sch/springboot/controller/SpringController.java
@@ -0,0 +1,20 @@
+package com.sch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SpringController {
+
+    @GetMapping("/spring")
+    public String spring(@RequestParam("name") String name,
+                         @RequestParam("age") int age,
+                         Model model) {
+        //model객체에 추가 --> view 전달
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "spring";  //view name
+    }
+}
